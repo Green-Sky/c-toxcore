2df6164e126329c1802c3773325d7684b126fcb9
Green Sky <Green-Sky@users.noreply.github.com>
2026-09-19 10:17:59 +0200

    fix(ngc): increase group announce store time
    Announces are only stored for `GCA_ANNOUNCE_SAVE_TIMEOUT` seconds and
    announced every `GC_SELF_REFRESH_ANNOUNCE_INTERVAL`.
    With timeout 30sec and interval 20min, you will only have a 30sec window
    every ~20min to find that peer. With this change that window grows 3x to
    90sec every ~20min.

--- toxcore/group_announce_test.cc
+++ toxcore/group_announce_test.cc
@@ -84,8 +84,8 @@ TEST_F(Announces, AnnouncesCanTimeOut)
     do_gca(mono_time_, gca_);
     ASSERT_NE(gca_->root_announces, nullptr);
 
-    // 29 seconds later, still there
-    advance_clock(29000);
+    // 89 seconds later, still there
+    advance_clock(89000);
     do_gca(mono_time_, gca_);
     ASSERT_NE(gca_->root_announces, nullptr);
 
